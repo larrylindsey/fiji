bd2e39ac480862fa3c71ef2fa1ccc9ac6746be3a
Larry Lindsey <llindsey@clm.utexas.edu>
2013-10-04 17:46:24 -0500

    TrakEM2 Archipelago: Log from node-TrakEM2 instances to the IJ log window

--- src-plugins/TrakEM2_Archipelago/src/main/java/edu/utexas/clm/archipelago/ijsupport/TrakEM2Archipelago.java
+++ src-plugins/TrakEM2_Archipelago/src/main/java/edu/utexas/clm/archipelago/ijsupport/TrakEM2Archipelago.java
@@ -23,20 +23,44 @@ import edu.utexas.clm.archipelago.FijiArchipelago;
 import edu.utexas.clm.archipelago.ijsupport.bottle.LayerBottler;
 import edu.utexas.clm.archipelago.ijsupport.bottle.PatchBottler;
 import edu.utexas.clm.archipelago.listen.ClusterStateListener;
+import edu.utexas.clm.archipelago.network.client.ArchipelagoClient;
 import ini.trakem2.ControlWindow;
 import ini.trakem2.Project;
 import ini.trakem2.persistence.FSLoader;
 import ini.trakem2.plugin.TPlugIn;
 import ini.trakem2.parallel.ExecutorProvider;
 import ini.trakem2.parallel.DefaultExecutorProvider;
+import ini.trakem2.utils.Utils;
 
 import java.io.File;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 
 public class TrakEM2Archipelago implements TPlugIn
 {
+    private static class LogStream extends PrintStream
+    {
+        private final ArchipelagoClient client;
+
+        public LogStream(OutputStream stream, ArchipelagoClient client)
+        {
+            super(stream);
+            this.client = client;
+        }
+
+        public void println(String str)
+        {
+            super.println(str);
+            if (client != null)
+            {
+                client.log(str);
+            }
+        }
+    }
+
     private class ProviderListener implements ClusterStateListener
     {
         private final AtomicBoolean doneSwitched;
@@ -144,6 +168,12 @@ public class TrakEM2Archipelago implements TPlugIn
             }
         }
         ControlWindow.setGUIEnabled(false);
+
+        if (!(Utils.getLogStream() instanceof LogStream ))
+        {
+            Utils.setLogStream(new LogStream(System.out, ArchipelagoClient.getFirstClient()));
+        }
+
         return Project.openFSProject(projectFile.getAbsolutePath(), false);
     }
 }
